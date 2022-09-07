89cefc6ae8b6791997a23c0706967220df59a4ef
DevEmmy <eolaosebikan60@gmail.com>
2022-09-07 14:30:46 +0100

    fixed documents bug

--- Routers/documentRoutes.js
+++ Routers/documentRoutes.js
@@ -6,10 +6,16 @@ const { compareSync } = require('bcrypt');
 const requireLogin = require('../middlewares/requireLogin.js');
 const Document = require('../Models/document.models.js');
 
+router.get("/all", async (req, res)=>{
+    Document.find()
+    .then(resp => res.json(resp))
+    .catch(err=> res.json(err))
+})
+
 router.post("/", requireLogin, async (req, res)=>{
     const document = req.body
     document.user = req.user
-    new Document.save(document)
+    new Document(document).save()
     .then(resp => res.json({message: "Uploaded successfully"}))
     .catch(err => res.json({message: "An error occured"}))
 })
